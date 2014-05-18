fda77fb84f9b25aff9c34234c4738aa3f69898e6
Nick White <nickswhite89@gmail.com>
2014-05-18 12:03:35 +0100

    First start at creating Portfolio page

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -3,13 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <title></title>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="{{ URL::asset('assets/css/template.css') }}">
 </head>
 <body>
-    <div class="container">
-        <div class="jumbotron">
-            <h1>Hello Word</h1>
-        </div>
-    </div>
+    <section class="home">
+        <header>
+            <h1>Nick White</h1>
+            <h4>Front-end Developer</h4>
+            <p>
+                Developer @rockabox working with AngularJs, Underscore, jQuery, SASS, Bootstrap 3 &amp; Bower.
+            </p>
+            <a href="" class="btn btn-default">About</a>
+        </header>
+    </section>
+    <section class="about jumbotron" id="about">
+        <h1>About</h1>
+        <p>
+            Graduate from University of Portsmouth, with a First Class (Hons) in Business Information Systems. Now working Rockabox, which is a Content Media company which creates and distributes online content for brands, broadcasters, publishers and retailers using its groundbreaking Shutters™ video solution and Viewshare distribution platform.
+        </p>
+        <p>
+            My current job role is as a Developer, working with PHP, Python, JavaScript, jQuery, Compass, HTML5, CSS3 and Git. Using all or a collection of these different skills to meet the clients desired end result. I have also had experience working with Project and Account Managers to provide written requirements for other developers to build from as well as working on IA and checking wireframes to ensure what is proposed is possible.
+        </p>
+    </section>
 </body>
 </html>
