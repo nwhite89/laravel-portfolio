5f874e716458015c79991967df3e64443aad4562
Nick White <nickswhite89@gmail.com>
2014-05-21 22:06:37 +0100

    About page
    - Start to create About pages

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -3,28 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <title></title>
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,400,400italic' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="{{ URL::asset('assets/css/template.css') }}">
 </head>
 <body>
-    <section class="home">
+    <section class="home dark" id="home">
         <header>
             <h1>Nick White</h1>
             <h4>Front-end Developer</h4>
             <p>
-                Developer @rockabox working with AngularJs, Underscore, jQuery, SASS, Bootstrap 3 &amp; Bower.
+                Developer <a href="http://www.rockabox.com" title="Rockabox Ltd.">@rockabox</a> working with
+                <a href="https://angularjs.org/" title="Angular JS">Angular JS</a>,
+                <a href="http://www.underscorejs.org" title="Underscore">Underscore</a>, <a href="http://jquery.com/" title="jQuery">jQuery</a>,
+                <a href="http://sass-lang.com/" title="Syntactically Awesome StyleSheets">SASS</a>,
+                <a href="http://compass-style.org/" title="Compass CSS Framework">Compass</a>, <a href="http://nodejs.org/">Node JS</a>,
+                <a href="http://getbootstrap.com/" title="Bootstrap 3">Bootstrap 3</a> &amp; <a href="http://www.bower.io" title="Bower Package Manager">Bower</a>.
             </p>
-            <a href="" class="btn btn-default">About</a>
         </header>
     </section>
-    <section class="about jumbotron" id="about">
-        <h1>About</h1>
-        <p>
-            Graduate from University of Portsmouth, with a First Class (Hons) in Business Information Systems. Now working Rockabox, which is a Content Media company which creates and distributes online content for brands, broadcasters, publishers and retailers using its groundbreaking Shutters™ video solution and Viewshare distribution platform.
-        </p>
-        <p>
-            My current job role is as a Developer, working with PHP, Python, JavaScript, jQuery, Compass, HTML5, CSS3 and Git. Using all or a collection of these different skills to meet the clients desired end result. I have also had experience working with Project and Account Managers to provide written requirements for other developers to build from as well as working on IA and checking wireframes to ensure what is proposed is possible.
-        </p>
+    <section class="about main light" id="about">
+        <div class="content">
+            <h1>A bit about me</h1>
+            <p>
+                Graduate from University of Portsmouth, with a First Class (Hons) in Business Information Systems. Now working at Rockabox, which is a Content Media company which creates and distributes online content for brands, broadcasters, publishers and retailers using its groundbreaking Shutters™ video solution and Viewshare distribution platform.
+            </p>
+            <p>
+                My current job role is as a Developer, working mainly on Front-end development with JavaScript, Angular JS, jQuery, SASS, Compass, HTML5, CSS3 and Git. Using all or a collection of these different skills to create different formats such as Shutters and working on the distribution platform RBX.
+            </p>
+        </div>
+    </section>
+    <section class="about secondary light">
+        <div class="row">
+            <div class="col-md-4">
+                <span class="glyphicon glyphicon-cloud"></span>
+            </div>
+            <div class="col-md-4">
+                2
+            </div>
+            <div class="col-md-4">
+                3
+            </div>
+        </div>
     </section>
 </body>
 </html>
